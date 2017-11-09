4882fadffec6053836adb4f87ff1b72e7b40c93d
Scarabol <scarabol@gmail.com>
2017-11-09 14:52:08 +0100

    increase version to 3.1

--- src/Construction.cs
+++ src/Construction.cs
@@ -33,7 +33,7 @@ namespace ScarabolMods
     [ModLoader.ModCallback (ModLoader.EModCallbackType.AfterStartup, "scarabol.construction.registercallbacks")]
     public static void AfterStartup ()
     {
-      Pipliz.Log.Write ("Loaded Construction Mod 3.0 by Scarabol");
+      Pipliz.Log.Write ("Loaded Construction Mod 3.1 by Scarabol");
       ManagerBlueprints.LoadBlueprints (Path.Combine (ModDirectory, "blueprints"));
     }
 
